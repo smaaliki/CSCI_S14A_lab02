8b0832eb9875ce693155e586bc8855499e1cf8b6
smaaliki <smaaliki@hotmail.com>
2019-07-09 00:15:18 +0400

    Moving model to index

--- routes.py
+++ routes.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
+	model = joblib.load('dregr.pkl')
 	prediction_input=[[4, 2.5, 3005, 15,17903.0,0,0,1]]
 	#prediction_input.reshape(1,-1)
 	prediction = model.predict(prediction_input).round(2)
 	return render_template("index.html", prediction="${:,.2f}".format(prediction[0]))
 
 if __name__ == "__main__":
-	model = joblib.load('dregr.pkl')
 	app.run(debug=True)
